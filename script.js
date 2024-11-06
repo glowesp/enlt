8c76207b18ebf43fd666d0569da5b932db566fef
glowesp <khunstr@protonmail.com>
2024-11-06 22:50:44 +0000

    push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+    var countDownDate = new Date("Nov 13, 2024 23:59:59").getTime();
 
     // Update the countdown every 1 second
     var x = setInterval(function() {
